3ebc18e3dd8cf0a1e81e8818ebd1c54004b3cb66
Conor Ryan <37616617+conorjcryan@users.noreply.github.com>
2019-04-22 01:03:55 +0100

    Update Database_Application.py

--- Database_Application.py
+++ Database_Application.py
@@ -27,7 +27,9 @@ def Add_Reading(Data):
         Min, Hour, Day, Month, Year = Timestamp.split(',')
         
         #SQL COMMAND, Inserts the new entry with the generated timestamp data
-        c.execute("INSERT INTO ConsumptionData (Reading,Minute,Hour,Day,Month,Year) VALUES (?,?,?,?,?,?)",(Data,Min,Hour,Day,Month,Year))   
+        c.execute(
+            "INSERT INTO ConsumptionData (Reading,Minute,Hour,Day,Month,Year) VALUES (?,?,?,?,?,?)",
+            (Data,Min,Hour,Day,Month,Year))   
         conn.commit()
         
         #Prints Success status message
@@ -42,39 +44,44 @@ def Add_Reading(Data):
 def Current_Load():
     
     #SQL COMMAND, sorts by most recent and only returns  2 results
-    c.execute("SELECT Minute,Hour FROM ConsumptionData ORDER BY Year DESC, Month DESC, Day DESC, Hour DESC, Minute DESC LIMIT 2")
+    c.execute(
+    "SELECT Minute,Hour FROM ConsumptionData ORDER BY Year DESC, Month DESC, Day DESC, Hour DESC, Minute DESC LIMIT 2")
     Result = c.fetchall()
     
     #Sorts the data
     MostRecent = Result[0]
     SecondRecent= Result[1]
-    print("Most Recent Entry: "+str(MostRecent))
-    print("Second Recent Entry: "+str(SecondRecent))
-    
+   
     #Unpacks the Most recent entry   
     X, Y = str(MostRecent).split(", ")
     M1 = X[1:3]
     H1 = Y[0:2]
-    print("Most Recent Timestamp: "+H1+":"+M1)
+    Timestamp = H1+":"+M1
     
     #Unpacks the Second recent entry 
     X, Y = str(SecondRecent).split(", ")
     M2 = X[1:3]
     H2 = Y[0:2]
-    print("Second Recent Timestamp: "+H2+":"+M2)
+    #print("Second Recent Timestamp: "+H2+":"+M2)
   
     #Calculate Time Taken
     Hours = int(H1) - int(H2)
     Minutes = int(M1) - int(M2)
     TimeTaken = Hours*60 + Minutes
-    print("Time Taken: "+str(TimeTaken)+" Minutes")
- 
-    #Current Load Formula
-    CurrentLoad = 1000*(60/TimeTaken)
-    print("Current Load is: "+str(int(CurrentLoad))+" Watts")
+    
+    if TimeTaken < 0 or TimeTaken > 600:
+    
+        print("Not Enough Recent Data to Calculate Load")
+        CurrentLoad = 0;
+        Timestamp = '00:00'
+        
+    else:
+
+        #Current Load Formula
+        CurrentLoad = int(1000*(60/TimeTaken))
   
     #Return the Result
-    return CurrentLoad
+    return CurrentLoad, Timestamp, Consumption_Today(), Consumption_This_Month()
 
 #Total kWh used today function
 def Consumption_Today():
@@ -87,12 +94,14 @@ def Consumption_Today():
     Day, Month, Year = Timestamp.split(',')
     
     #SQL COMMAND, returns all entries today
-    c.execute("SELECT * FROM ConsumptionData WHERE Day=? AND Month=? AND Year=?",(Day,Month,Year,))
+    c.execute(
+        "SELECT * FROM ConsumptionData WHERE Day=? AND Month=? AND Year=?",
+        (Day,Month,Year,))
     Result = c.fetchall()
     
     #Calculate Consumption Today
-    Consumption = len(Result)
-    print(str(Consumption)+"kWh Used Today")
+    x = len(Result)
+    Consumption = "{0:0=2d}".format(x)
     
     #Return the Result
     return Consumption
@@ -108,12 +117,14 @@ def Consumption_This_Month():
     Month, Year = Timestamp.split(',')
     
     #SQL COMMAND, returns all entries this month
-    c.execute("SELECT * FROM ConsumptionData WHERE Month=? AND Year=?",(Month,Year,))
+    c.execute(
+        "SELECT * FROM ConsumptionData WHERE Month=? AND Year=?",
+        (Month,Year,))
     Result = c.fetchall()
     
     #Calculate Consumption Today
-    Consumption = len(Result)
-    print(str(Consumption)+"kWh Used This Month")
+    x = len(Result)
+    Consumption = "{0:0=3d}".format(x)
     
     #Return the Result
     return Consumption
@@ -125,7 +136,9 @@ def Day_Graph(Day,Month,Year):
     if Day in range(1,31) and Month in range(1,12):
         
         #SQL COMMAND, returns the hour data from all entries today
-        c.execute("SELECT Hour FROM ConsumptionData WHERE Day=? AND Month=? AND Year=?",(Day,Month,Year,))
+        c.execute(
+            "SELECT Hour FROM ConsumptionData WHERE Day=? AND Month=? AND Year=?",
+            (Day,Month,Year,))
         Result = c.fetchall()
         
         ##Creates an empty String
@@ -135,14 +148,15 @@ def Day_Graph(Day,Month,Year):
         for Hour in range(0,24):
             
             #Counts the number of entries in the specified hour
-            Count = Result.count((Hour,))
             
+            x = Result.count((Hour,))
+            Count = "{0:0=2d}".format(x)
             #checks if the final hour has been reached
             if Hour == 23:                
                 Data = Data + str(Count)      
             #if not, continue apending the string
             else:                
-                Data = Data + str(Count) + ", "
+                Data = Data + str(Count) + ","
       
     #prints Error Message
     else:       
@@ -150,7 +164,6 @@ def Day_Graph(Day,Month,Year):
      
     #Convert to a string and display the data
     Consumption = "("+str(Data)+")" 
-    print("Hourly Consumption for "+str(Day)+"/"+str(Month)+"/"+str(Year)+" is "+Consumption)
     
     #Return the Result
     return Consumption
@@ -162,7 +175,9 @@ def Month_Graph(Month,Year):
     if Month in range(1,12):
         
         #SQL COMMAND, returns the day data from all entries this month
-        c.execute("SELECT Day FROM ConsumptionData WHERE Month=? AND Year=?",(Month,Year,))
+        c.execute(
+            "SELECT Day FROM ConsumptionData WHERE Month=? AND Year=?",
+            (Month,Year,))
         Result = c.fetchall()
         
         ##Creates an empty String
@@ -172,14 +187,15 @@ def Month_Graph(Month,Year):
         for Day in range(1,32):
             
             #Counts the number of entries in the specified day
-            Count = Result.count((Day,))
-              
+            x = Result.count((Day,))
+            Count = "{0:0=2d}".format(x)
+            
             #checks if the final day has been reached
             if Day == 31:                
                 Data = Data + str(Count)
             #if not, continue apending the string
             else:                
-                Data = Data + str(Count) + ", "
+                Data = Data + str(Count) + ","
      
     #prints Error Message
     else:       
@@ -187,20 +203,40 @@ def Month_Graph(Month,Year):
     
     #Convert to a string and display the data
     Consumption = "("+str(Data)+")" 
-    print("Daily Consumption for "+str(Month)+"/"+str(Year)+" is "+Consumption)
     
     #Return the Result
     return Consumption
 
 print("K00211753 - Energy Monitoring Device online")
 
-##########USED FOR TESTING FUNCTIUONS##########
-#Add_Reading(17)
-#c.execute("SELECT * FROM ConsumptionData")
-#All_Data = c.fetchall()
-#print("All Records: "+str(All_Data)+"\n")
-#Current_Load()
-#Consumption_Today()
-#Consumption_This_Month()
-#Day_Graph(20,4,2019)
-#Month_Graph(4,2019)
+#Add_Reading(1)
+
+Database_Socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+Database_Socket.bind(('',5000))
+
+while True:
+
+    print("Waiting for Requests..........")
+    Request, Source = Database_Socket.recvfrom(1024)
+    Address = (Source[0], 9998)
+    
+    if 'Now' in str(Request):
+        
+        Data = Current_Load()
+        print("NOW FRAGMENT ACTIVE! - Current Load: "+str(Data[0])+"w - Timestamp: "+str(Data[1])+" - Consumption  Today: "+str(Data[2])+"kWh - Consumption This Month: "+str(Data[3])+"kWh")
+        Reply = str(Data).encode()
+        Database_Socket.sendto(Reply, Address)
+
+    elif 'Day' in str(Request):
+        
+        Data = Day_Graph(20,4,2019)
+        print("DAY FRAGMENT ACTIVE - Consumption Today per Hour: "+str(Data))
+        Message = str(Data).encode()
+        Database_Socket.sendto(Message, Address)
+        
+    elif 'Month' in str(Request):
+        
+        Data = Month_Graph(4,2019)
+        print("MONTH FRAGMENT ACTIVE - Consumption This Month per Day: "+str(Data))
+        Message = str(Data).encode()
+        Database_Socket.sendto(Message, Address)
